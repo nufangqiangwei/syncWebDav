cf797706a461d117bd36d8b837106437f22cfd69
qiangwei <email.com>
2023-01-08 17:54:27 +0800

    老板还在咳嗽

--- lib/pkg/save/client.dart
+++ lib/pkg/save/client.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as path;
+
 class Store {
   late String _fromTable = '';
   final List<Method> _whereArgs = [];
+  Store(){getDb();}
 
   // 构建表达式
   Store select(List<Method>? args) {
@@ -24,7 +26,7 @@ class Store {
   }
 
   // 查询结果
-  Future<List<Map<String,dynamic>>> all<T>() async {
+  Future<List<Map<String, dynamic>>> all<T>() async {
     if (_fromTable == "") {
       _fromTable = T.toString();
     }
@@ -41,73 +43,99 @@ class Store {
 
   save() async {}
 
-  DB getDb(){
-    return DB.getInstance();
+  DB getDb() {
+    DB db = DB.getInstance();
+    db.init();
+    return db;
   }
 }
 
 class DB {
   static DB? _instance;
-  Map<String,dynamic> data = {};
-  late bool _isSave =false;
-  late String filePath="";
-  late String saveType="";
+  Map<String, dynamic> data = {};
+  late bool _isSave = false;
+  late String filePath = "";
+  late String saveType = "";
+
   init() async {
-    if(kIsWeb ) {
+    if (saveType != "") {
+      return;
+    }
+    if (kIsWeb) {
       saveType = "cookies";
-      throw("未知的web平台");
-    }else if (Platform.isAndroid||Platform.isIOS||Platform.isWindows||Platform.isLinux||Platform.isMacOS) {
+      throw ("未知的web平台");
+    } else if (Platform.isAndroid ||
+        Platform.isIOS ||
+        Platform.isWindows ||
+        Platform.isLinux ||
+        Platform.isMacOS) {
       saveType = "file";
       var value = await getApplicationDocumentsDirectory();
-      filePath=path.join(value.path,"data");
-    }else if (Platform.isFuchsia){
-      throw("未知的平台");
+      filePath = path.join(value.path, "data");
+    } else if (Platform.isFuchsia) {
+      throw ("未知的平台");
     }
-
+    print(filePath);
     File f = File(filePath);
-    data = jsonDecode(f.readAsStringSync());
+    if (!await f.exists()){
+      await f.create();
+      await f.writeAsString("{}");
+    }
+    String i = await f.readAsString();
+    print("文本内容是$i");
+    data = jsonDecode(i);
   }
 
   DB._();
 
   static DB getInstance() {
-
     _instance ??= DB._();
     return _instance!;
   }
 
-  save(){
+  save() {
+    if (saveType == "") {
+      throw ("尚未初始化");
+    }
     _isSave = true;
     File f = File(filePath);
     f.writeAsString(jsonEncode(data));
 
     _isSave = false;
   }
-  getBucket(String key){
-    if (saveType==""){
-      throw("尚未初始化");
+
+  getBucket(String key) {
+    if (saveType == "") {
+      throw ("尚未初始化");
     }
-    if(_isSave){
-      return ;
+    if (_isSave) {
+      return;
     }
-    return data[key];
+    dynamic i = data[key];
+    if (i == null) {
+      List<Map<String, dynamic>> x=[];
+      i = x;
+    }
+    //
+    // i ??= x;
+    return i;
   }
 
-  List<Map<String,dynamic>> selectData(String key, List<Method> methods){
-    var bucket = getBucket(key) as List<Map<String,dynamic>>;
-    List<Map<String,dynamic>> result = [];
-    for (var i =0;i<bucket.length;i++){
-      if (!checkData(bucket[i],methods)){
-        continue ;
+  List<Map<String, dynamic>> selectData(String key, List<Method> methods) {
+    var bucket = getBucket(key) as List<Map<String, dynamic>>;
+    List<Map<String, dynamic>> result = [];
+    for (var i = 0; i < bucket.length; i++) {
+      if (!checkData(bucket[i], methods)) {
+        continue;
       }
       result.add(bucket[i]);
     }
     return result;
   }
 
-  bool checkData(Map<String,dynamic> data, List<Method> methods){
-    for(final i in methods) {
-      if (!i.check(data)){
+  bool checkData(Map<String, dynamic> data, List<Method> methods) {
+    for (final i in methods) {
+      if (!i.check(data)) {
         return false;
       }
     }
@@ -115,12 +143,10 @@ class DB {
   }
 }
 
-
-class User{
+class UserModel {
   static bool isOnly = true;
   static DbString userId =
-      DbString(table: "User", field: "userId", defaultValue: "11");
-
+  DbString(table: "User", field: "userId", defaultValue: "11");
 }
 
 class Method {
@@ -128,8 +154,14 @@ class Method {
   final String field;
   final String method;
   final dynamic value;
-  Method({required this.table,required this.field, required this.method, required this.value});
-  check(Map<String,dynamic> data){
+
+  Method(
+      {required this.table,
+        required this.field,
+        required this.method,
+        required this.value});
+
+  check(Map<String, dynamic> data) {
     var value = data[field];
     switch (method) {
       case "equal":
@@ -140,31 +172,31 @@ class Method {
   }
 
   bool equal(dynamic data) {
-    if (data is String){
+    if (data is String) {
       value as String;
       return data == value;
     }
-    if(data is int) {
+    if (data is int) {
       value as int;
       return data == value;
     }
-    if(data is double) {
+    if (data is double) {
       value as double;
       return data == value;
     }
     return false;
   }
 
-  bool moreThan(dynamic data){
-    if (data is String){
+  bool moreThan(dynamic data) {
+    if (data is String) {
       value as String;
       return data.hashCode <= value.hashCode;
     }
-    if(data is int) {
+    if (data is int) {
       value as int;
       return data <= value;
     }
-    if(data is double) {
+    if (data is double) {
       value as double;
       return data <= value;
     }
@@ -176,12 +208,12 @@ class DbString {
   final String table;
   final String field;
   final String defaultValue;
-  late String value='';
+  late String value = '';
 
   DbString({required this.table, required this.field, this.defaultValue = ''});
 
   Method equal(value) {
-    return Method(table:table,field: field,method: "equal",value: value);
+    return Method(table: table, field: field, method: "equal", value: value);
   }
 }
 
@@ -193,7 +225,4 @@ class DbInt {
   DbInt({required this.table, required this.field, this.defaultValue = ''});
 }
 
-t() async {
-  Store a = Store();
-  var b = await a.select([]).from("").orderBy().all<User>();
-}
+
